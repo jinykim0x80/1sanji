2a31a38f9170c48bbebdbba7cb0950fd8981a7aa
cafemoca <cafemoca@localhost.localdomain>
2019-01-22 19:24:15 +0900

    smart contract skeleton file

---
+++ onesanji.cpp
@@ -0,0 +1,98 @@
+#include "onesanji.hpp"
+void onesanji::addrice(
+		uint64_t order_no, 
+		std::string brand, 
+		std::string kind, 
+		std::string volume, 
+		uint32_t price, 
+		std::string producer, 
+		std::string locate_address,
+		uint32_t insurance_day,
+		uint32_t harvest_day,
+		uint32_t cutting_day,
+		std::string recipient,
+		uint32_t phone_number)
+{
+	grice_index rice_gold_table(_self,_self);
+	eosio_assert(rice_gold_table.get_code() == _self, "you don't access");
+	eosio::print("addrice called\n");
+	eosio::print("emplace rice_gold_table\n");
+	auto iter = rice_gold_table.find(order_no);
+	eosio_assert(iter == rice_gold_table.end(), "order_no already exist");
+//	grice_index rice_gold_table(_self,_self);
+	rice_gold_table.emplace(_self, [&](auto& r){
+		r.order_no = order_no;
+		r.brand = brand;
+		r.kind = kind;
+		r.volume = volume;
+		r.price = price;
+		r.producer = producer;
+		r.locate_address = locate_address;
+		r.insurance_day = insurance_day;
+		r.harvest_day = harvest_day;
+		r.cutting_day = cutting_day;
+		r.recipient = recipient;
+		r.phone_number = phone_number;
+	});
+}
+
+void onesanji::delrice(uint64_t order_no, const std::string qrtype/* false = black, true = gold*/){
+	eosio_assert(qrtype == "black" || qrtype == "gold", "Invalid qrtype");
+	eosio::print("delrice called\n");
+	if(qrtype == "black"){
+		print("It is gold\n");
+	}
+	else if(qrtype == "gold"){
+		//grice_index rice_gold_table(_self, _self);
+		//
+		grice_index rice_gold_table(_self,_self);
+		eosio_assert(rice_gold_table.get_code() == _self, "you don't access");
+
+		auto iter = rice_gold_table.find(order_no);
+		eosio_assert(iter != rice_gold_table.end(), "order_no doen't exist");
+		rice_gold_table.erase(iter);
+	}
+}
+void onesanji::viewrice(uint64_t order_no, const std::string qrtype /* false = black, true = gold*/){
+	eosio_assert(qrtype == "black" || qrtype == "gold", "Invalid qrtype");
+	eosio::print("viewrice called\n");
+	if(qrtype == "black"){
+
+	}
+	else if(qrtype == "gold"){
+		//grice_index rice_gold_table(_self,_self);
+		//eosio_assert(rice_gold_table.get_code() == "onesanji1234"_n, "you don't access");
+		grice_index rice_gold_table(_self,_self);
+		
+		auto iter = rice_gold_table.find(order_no);
+		eosio_assert(iter != rice_gold_table.end(), "order_no not found");
+		eosio::print("order_no: ", iter->order_no, "\n");	
+		eosio::print("brand: ", iter->brand, "\n");	
+		eosio::print("volume: ", iter->volume, "\n");	
+		eosio::print("kind: ", iter->kind, "\n");	
+		eosio::print("price: ", iter->price, "\n");	
+		eosio::print("producer: ", iter->producer, "\n");	
+		eosio::print("locate_address: ", iter->locate_address, "\n");	
+		eosio::print("insurance_day: ", iter->insurance_day, "\n");	
+		eosio::print("harvest_day: ", iter->harvest_day, "\n");	
+		eosio::print("cutting_day: ", iter->cutting_day, "\n");	
+		eosio::print("recipient: ", iter->recipient, "\n");	
+		eosio::print("phone_number: ", iter->phone_number, "\n");		
+		eosio::print("printing finish\n");
+	}
+}
+
+void onesanji::printalltable() {
+	//grice_index rice_gold_table(_self,_self);
+	grice_index rice_gold_table(_self,_self);
+	eosio_assert(rice_gold_table.get_code() == _self, "you don't access");
+	eosio::print("pprintalltable called\n");
+	std::vector<uint64_t> primary_keys(0);
+	for(const auto& i : rice_gold_table){
+		primary_keys.push_back(i.primary_key());	
+	}
+	for(const auto& i : primary_keys){
+		viewrice(i, "gold");		
+	}
+}
+EOSIO_ABI(onesanji, (addrice)(delrice)(viewrice)(printalltable))
---
+++ onesanji.hpp
@@ -0,0 +1,121 @@
+#pragma once
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/system.h>
+#include <eosiolib/serialize.hpp>
+
+
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+using namespace eosio;
+
+class onesanji : contract {
+using contract::contract;
+private:
+	/// abi table 
+
+	struct [[eosio::table]] blackqr{
+		uint64_t order_no;						// uniquekey
+		std::string brand;					// 브랜드
+		std::string kind;					
+		std::string volume;					// 용량
+		uint32_t price;					// 가격
+		std::string producer;				// 생산자
+		std::string locate_address;			// 지역주소
+		uint32_t insurance_day;		    // 데이터 보장기간 yymmdd < 2^32
+
+		auto primary_key() const { return order_no; }	
+		auto get_insurance_day() const { return insurance_day; }
+	
+		uint32_t harvest_day;				// 수확일
+		uint32_t cutting_day;				// 도정일
+		EOSLIB_SERIALIZE(blackqr, (order_no)(brand)(kind)(volume)(price)(producer)(locate_address)(insurance_day)(harvest_day)(cutting_day))
+	};
+	struct [[eosio::table]] goldqr{
+		uint64_t order_no;						// uniquekey
+		std::string brand;					// 브랜드
+		std::string kind;					
+		std::string volume;					// 용량
+		uint32_t price;					// 가격
+		std::string producer;				// 생산자
+		std::string locate_address;			// 지역주소
+		uint32_t insurance_day;		    // 데이터 보장기간 yymmdd < 2^32
+		uint32_t harvest_day;				// 수확일
+		uint32_t cutting_day;				// 도정일
+		std::string recipient;				// 받는사람
+		uint32_t phone_number;	        // 휴대폰번호 뒷자리 
+		auto get_phone_number() const { return phone_number; }
+
+		auto primary_key() const { return order_no; }	
+		auto get_insurance_day() const { return insurance_day; }
+		EOSLIB_SERIALIZE(goldqr, (order_no)(brand)(kind)(volume)(price)(producer)(locate_address)(insurance_day)(harvest_day)(cutting_day)(recipient)(phone_number))
+	};
+
+	typedef eosio::multi_index<"goldqr"_n, goldqr> grice_index;
+	//grice_index rice_gold_table;
+	/*
+	void check(){
+		//eosio_assert(rice_gold_table.get_code() == "tester"_n, "You don't access");
+	}
+	*/
+public:
+	onesanji(account_name self) : contract(self)/*, rice_gold_table(self, self) */{}
+
+	[[eosio::action]] void addrice(
+		uint64_t order_no, 
+		std::string brand, 
+		std::string kind, 
+		std::string volume, 
+		uint32_t price, 
+		std::string producer, 
+		std::string locate_address,
+		uint32_t insurance_day,
+		uint32_t harvest_day,
+		uint32_t cutting_day,
+		std::string recipient,
+		uint32_t phone_number);
+	// for black
+	// /*
+//	[[eosio::action]] void addRice(
+//		std::string /*order_no*/, 
+//		std::string /*brand*/, 
+//		std::string /*kind*/, 
+//		std::string /*volume*/, 
+//		size_t /*price*/, 
+//		std::string /*producer*/, 
+//		std::string /*locate_address*/,
+//		std::string /*insurance_day*/			// insurance_day casting to uint32_t
+//	);					
+//
+//	// for gold 
+//	[[eosio::action]] void addRice(
+//		std::string /*order_no*/, 
+//		std::string /*brand*/, 
+//		std::string /*kind*/, 
+//		std::string /*volume*/, 
+//		size_t /*price*/, 
+//		std::string /*producer*/, 
+//		std::string /*locate_address*/,
+//		std::string /*insurance_day*/,			// insurance_day casting to uint32_t
+//		std::string /*harvest_day*/,
+//		std::string /*cutting_day*/,
+//		std::string /*recipient*/,
+//		uint32_t /*phone_number*/
+//	); 
+//
+	[[eosio::action]] void delrice(uint64_t/*order_no*/, const std::string /*qrtype*/);
+//	[[eosio::action]] void collectResource();
+	[[eosio::action]] void viewrice(uint64_t /*order_no*/, const std::string /*qrtype*/);
+	[[eosio::action]] void printalltable() ;
+	/*
+	const int currentDateTime(){
+		timet     now = std::clock();
+		struct tm  tstruct;
+		char       buf[8];
+		tstruct = *localtime(&now);
+		strftime(buf, sizeof(buf), "%g%m%d", &tstruct);
+
+		return atoi(buf);
+	}
+	*/
+};
+
